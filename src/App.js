0f8a85024f5faed4767d1c20f36b55b246945403
Agnieszka Komorowska <agn.komorowska@gmail.com>
2022-04-30 19:34:02 +0200

    add pages, install dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Articles from './components/articles/Articles';
-import Article from './components/articles/Article';
 import Navigation from './components/navigation/Navigation';
 import Home from './components/home/Home';
+import About from './components/about/About';
+import Timeline from './components/timeline/Timeline';
+import Projects from './components/projects/Projects';
+import Skills from './components/skills/Skills';
+import Contact from './components/contact/Contact';
+import Articles from './components/articles/Articles';
+import Article from './components/articles/Article';
 import NotFound from './components/notFound/NotFound';
 
 function App() {
@@ -11,8 +16,16 @@ function App() {
     <BrowserRouter>
       <Navigation />
       <Routes>
+
+        <Route path="/about" element={<About />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+
         <Route path="/articles" element={<Articles />} />
         <Route path="/article" element={<Article />} />
+
         <Route exact path="/" element={<Home />}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -8,6 +8,12 @@ export default class Navigation extends React.Component {
             <nav>
                 <ul>
                     <li> <Link to="/">Home</Link></li>
+                    <li> <Link to="/about">About</Link></li>
+                    <li> <Link to="/timeline">Timeline</Link></li>
+                    <li> <Link to="/skills">Skills</Link></li>
+                    <li> <Link to="/projects">Projects</Link></li>
+                    <li> <Link to="/contact">Contact</Link></li>
+
                     <li> <Link to="/articles">Articles</Link></li>
                     <li> <Link to="/alwwid">Bad Link</Link></li>
                 </ul>
