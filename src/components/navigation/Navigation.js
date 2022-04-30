ea39dfb030446c9f1664094ca8acf71a75f8907f
Agnieszka Komorowska <agn.komorowska@gmail.com>
2022-04-30 21:00:24 +0200

    remove brand from navbar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,14 +5,13 @@ export default class Navigation extends React.Component {
 
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item"> <Link className="nav-link" to="/">Home</Link> </li>
                         <li class="nav-item"> <Link className="nav-link" to="/">Home</Link> </li>
                         <li class="nav-item"> <Link className="nav-link" to="/about">About</Link> </li>
