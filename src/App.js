c966813eec3a0e90701fa4c055095049644bcd51
Agnieszka Komorowska <agn.komorowska@gmail.com>
2022-05-06 00:51:56 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navigation from './components/navigation/Navigation';
+import Navigation from './components/common/Navigation';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Timeline from './components/timeline/Timeline';
 import Projects from './components/projects/Projects';
 import Skills from './components/skills/Skills';
 import Contact from './components/contact/Contact';
-import Articles from './components/articles/Articles';
-import Article from './components/articles/Article';
 import NotFound from './components/notFound/NotFound';
 
 function App() {
@@ -21,10 +19,6 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/contact" element={<Contact />} />
-
-        <Route path="/articles" element={<Articles />} />
-        <Route path="/article" element={<Article />} />
-
         <Route exact path="/" element={<Home />}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
